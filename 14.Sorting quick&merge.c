458b8fa08bfc3c70ea6a4aa0c97f86e1e4aa785e
achari ganesh <11249a002@kanchiuniv.ac.in>
2025-12-01 08:24:31 +0530

    14.Sorting quick&merge.c

---
+++ 14.Sorting quick&merge.c
@@ -0,0 +1,75 @@
+
+    AIM:
+    To sort an array of integers using the Quick Sort algorithm in C.
+
+
+ Program:
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array (Quick Sort): ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
+Sample output:
+Enter number of elements: 5
+Enter 5 elements:
+40 10 80 20 60
+Sorted array (Quick Sort): 10 20 40 60 80
+
+
+
+ RESULT:
+    The program successfully sorts the given set of numbers using Quick Sort
+    and displays the sorted array.
